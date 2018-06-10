42a4a4edf3c6bdc5d6e12a1646e09c9b7f1bb1c3
Sidd-GrizzAp3 <its.sidd.artha@gmail.com>
2018-06-10 10:55:30 -0400

    seems to be working

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+  
+@app.route("/first_route")
+def first_route():
+    return "Hello From the Other Side"
+
+@app.route("/first_route/<param>")
+def first(param):
+    return param
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
